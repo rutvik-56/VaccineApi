35b870d680714d4367fc3fe02d0846bbf9e1cc7c
Rutvik <Rutvik@DESKTOP-B8A659H>
2021-05-04 00:37:34 +0530

    api for aleart of Vaccine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'forVaccine';
+export class AppComponent{
+  
+  constructor(private http: HttpClient)
+  {
+
+  }
+
+  fetched:any = "hello";
+  result : any;
+  custom()
+  {
+    this.http.get<any>('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=854334&date=04-05-2021').subscribe(
+      
+      value => {
+           this.fetched = value;
+           if(this.fetched["sessions"].length!=0)
+           {
+               // play music
+               this.result.unsubscribe();
+           }
+          else{
+               console.log("not find out");
+           }
+         }
+     );
+  }
+
+  call()
+  {
+    this.result = interval(30000).subscribe((func => {
+      this.custom();
+    }))
+  }
+  
+
 }
