2b71ad679a143720d649509777e03f60f1433aa6
Rutvik <Rutvik@DESKTOP-B8A659H>
2021-05-04 10:14:09 +0530

    music added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,13 +18,17 @@ export class AppComponent{
   result : any;
   custom()
   {
-    this.http.get<any>('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=854334&date=04-05-2021').subscribe(
+    this.http.get<any>('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=395006&date=05-05-2021').subscribe(
       
       value => {
            this.fetched = value;
            if(this.fetched["sessions"].length!=0)
            {
-               // play music
+                // play music
+                let audio = new Audio();
+                audio.src = "../../../assets/audio/file.mp3";
+                audio.load();
+                audio.play();
                this.result.unsubscribe();
            }
           else{
@@ -39,6 +43,8 @@ export class AppComponent{
     this.result = interval(30000).subscribe((func => {
       this.custom();
     }))
+
+    
   }
   
 
